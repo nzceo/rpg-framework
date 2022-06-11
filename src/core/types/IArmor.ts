92159a7c3364e8e328099d073d5a7820b551b031
nzceo <70579261+nzceo@users.noreply.github.com>
2022-06-11 12:26:48 +0100

    Initial armor improvements

--- src/core/types/IArmor.ts
+++ src/core/types/IArmor.ts
@@ -1,4 +1,33 @@
+import Game from "../game";
+
 export interface IArmor {
+  /**
+   * The armor's name
+   */
   name: string;
+  /**
+   * How much the armor protects the wearer
+   */
   damageThreshold: number;
+  /**
+   * The armor's baseprice, buy and sell price derive from this
+   * as well as merchant settings
+   */
+  basePrice: number;
+  /**
+   * Whether the armor has been tailored or not.
+   * A tailored armor will fit when pregnant.
+   */
+  tailored: boolean;
+  /**
+   * Function to be used every time the player needs to wear clothes (e.g. every morning)
+   * The function will return `result` to see if player can still wear it as well as `message`,
+   * a more descriptive outcome of the result.
+   */
+  wear: (
+    game: Game
+  ) => {
+    result: boolean;
+    message: string;
+  };
 }
--- src/gameData/static/armors.ts
+++ src/gameData/static/armors.ts
@@ -5,7 +5,15 @@ const armors: {
 } = {
   clothes: {
     name: "clothes",
-    damageThreshold: 1
+    damageThreshold: 1,
+    basePrice: 1,
+    tailored: false
+  },
+  leatherArmor: {
+    name: "leather armor",
+    damageThreshold: 12,
+    basePrice: 20,
+    tailored: false
   }
 };
 
